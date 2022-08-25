002839e7e2c8e8a7e7a155bbfe57e72c07e91f4a
PatriciaDivi <patriciavianad@gmail.com>
2022-08-25 17:50:39 -0300

    fix: :bug: correção de bug

--- app/backend/src/middlewares/loginValidateMiddlewares.ts
+++ app/backend/src/middlewares/loginValidateMiddlewares.ts
@@ -1,14 +1,13 @@
 import { Request, Response, NextFunction } from 'express';
 import { CustomerError } from '../utils/customerError';
+import { regexEmail } from '../utils/emailRegex';
 
 const loginValidateMiddlewares = (req: Request, _res: Response, next: NextFunction) => {
   const { email, password } = req.body;
 
-  const regex = new RegExp('^[_a-z0-9-]+(.[_a-z0-9-]+)*@[a-z0-9-]+(.[a-z0-9-]+).(.[a-z]{2,3})$');
-
   if (!email || !password) throw new CustomerError(400, 'All fields must be filled');
 
-  if (!regex.test(email) || password.length < 6) throw new CustomerError(401, 'Incorrect email or password');
+  if (!regexEmail.test(email) || password.length < 6) throw new CustomerError(401, 'Incorrect email or password');
 
   next();
 };
