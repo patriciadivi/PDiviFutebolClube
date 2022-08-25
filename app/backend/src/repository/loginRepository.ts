6d714a97d1e8852adc69d7eb553d4b62ed4765d8
PatriciaDivi <patriciavianad@gmail.com>
2022-08-25 02:48:48 -0300

    feat: :sparkles: Desenvolvimento da divisão para a requisição na model (banco de dados)

---
+++ app/backend/src/repository/loginRepository.ts
@@ -0,0 +1,12 @@
+import UserModel from '../database/models/UserModel';
+import { IUserInterface } from '../interfaces/IUserInterface';
+
+export default class LoginRepository {
+  private userModel = UserModel
+
+  public resultLogin = async (email: string): Promise<IUserInterface> => {
+    const resultUser = await this.userModel.findOne({ where: { email }, raw: true });
+
+    return resultUser as IUserInterface;
+  }
+}
