b49112d350bba94829926d104bf2cc8fc16d96b2
PatriciaDivi <patriciavianad@gmail.com>
2022-08-29 15:21:28 -0300

    style: :lipstick: Correção no nome a const 'routerTeams'

--- app/backend/src/routes/teams.routes.ts
+++ app/backend/src/routes/teams.routes.ts
@@ -1,9 +1,9 @@
 import { Router } from 'express';
 import TeamsController from '../controllers/teamsController';
 
-const roguterTeams = Router();
+const routerTeams = Router();
 
-roguterTeams.get('/', TeamsController.ControllerTeamsAll);
-roguterTeams.get('/:id', TeamsController.ControllerTeamsId);
+routerTeams.get('/', TeamsController.ControllerTeamsAll);
+routerTeams.get('/:id', TeamsController.ControllerTeamsId);
 
-export default roguterTeams;
+export default routerTeams;
