4b5506749c0419f0d71d441b5d62b5445c339169
PatriciaDivi <patriciavianad@gmail.com>
2022-08-29 14:20:13 -0300

    feat(Iniciado pasta 'Routes' para gerenciamento das rotas): :sparkles: Criado rota get para 'temas/id'

--- app/backend/src/routes/teams.routes.ts
+++ app/backend/src/routes/teams.routes.ts
@@ -4,5 +4,6 @@ import TeamsController from '../controllers/teamsController';
 const roguterTeams = Router();
 
 roguterTeams.get('/', TeamsController.ControllerTeamsAll);
+roguterTeams.get('/:id', TeamsController.ControllerTeamsByAppId);
 
 export default roguterTeams;
