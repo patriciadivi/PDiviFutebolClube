e70fb682ae08e03d5f3a2463e6cfc20b696eb538
PatriciaDivi <patriciavianad@gmail.com>
2022-08-29 14:23:12 -0300

    feat: :sparkles: Desenvolvido método 'controllerTeamsByAppId'

--- app/backend/src/controllers/teamsController.ts
+++ app/backend/src/controllers/teamsController.ts
@@ -2,12 +2,20 @@ import { Request, Response } from "express";
 import TeamsService from '../services/teamsService';
 
 class TeamsController {
-  
-   ControllerTeamsAll = async (req: Request, res: Response) => {
+
+  ControllerTeamsAll = async (_req: Request, res: Response) => {
     const resultTeamsServiceAll = await TeamsService.ServiceTeamsAll()
     console.log('resultTeamsServiceAll', resultTeamsServiceAll);
     
     return res.status(200).json(resultTeamsServiceAll);
+  };
+
+  ControllerTeamsByAppId = async (req: Request, res: Response) => {
+    const { id } = req.params;
+    // console.log(req.params);
+    const resultTeamsServiceId = await TeamsService.ServiceTeamsByAppId(id)
+    return res.status(200).json(resultTeamsServiceId);
+
   };
 }
 
