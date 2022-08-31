8a9321e9132d50641baa61952304f82d3c8fe44b
PatriciaDivi <patriciavianad@gmail.com>
2022-08-31 15:51:34 -0300

    feat: :sparkles: Camada de repository para o acesso a model 'Matches'
    Desenvolvido metodo 'RepositoryMatchesCreate' para criar um partida
    
    Desenvolvido metodo 'RepositoryMatchesFinish' para atualização

--- app/backend/src/repository/matchesRepository.ts
+++ app/backend/src/repository/matchesRepository.ts
@@ -2,7 +2,6 @@ import Teams from '../database/models/TeamsModel';
 import Matches from '../database/models/MatchesModel';
 
 class MatchesRepository {
-
   public RepositoryMatchesAll = async () => {
     const resultMatchesModelAll = await Matches.findAll({
       include: [
@@ -10,7 +9,7 @@ class MatchesRepository {
         { model: Teams, as: 'teamAway', attributes: ['teamName'] },
       ],
     });
-    return resultMatchesModelAll
+    return resultMatchesModelAll;
   };
 
   public RepositoryMatchesInProgress = async (inPro: boolean) => {
@@ -19,10 +18,19 @@ class MatchesRepository {
       include: [
         { model: Teams, as: 'teamHome', attributes: ['teamName'] },
         { model: Teams, as: 'teamAway', attributes: ['teamName'] },
-        ],
+      ],
     });
     return resultMatchesModelInProgress;
   };
+
+  public RepositoryMatchesCreate = async (newMtcheCreate: object) => {
+    const resultCreate = Matches.create(newMtcheCreate);
+    console.log('created >>>>>>>>>>', resultCreate);
+    return resultCreate;
+  };
+
+  public RepositoryMatchesFinish = async (finishId: number) =>
+    Matches.update({ inProgress: false }, { where: { id: finishId } });
 }
 
 export default new MatchesRepository();
