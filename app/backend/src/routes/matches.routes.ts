2073032546c20e2ae987515ebfae4679bddfab18
PatriciaDivi <patriciavianad@gmail.com>
2022-08-31 15:39:00 -0300

    feat: :sparkles: Rotas post e patch criadas
    Rota post '/matches' criada para a criação de novos timer
    
    Rota de patch 'matches/:id/finish' pata atualização

--- app/backend/src/routes/matches.routes.ts
+++ app/backend/src/routes/matches.routes.ts
@@ -1,8 +1,11 @@
 import { Router } from 'express';
+import validateToken from '../middlewares/validateToken';
 import Matchescontroller from '../controllers/matchesController';
 
 const routesMatches = Router();
 
 routesMatches.get('/', Matchescontroller.ControllerMatchesAll);
+routesMatches.post('/', validateToken, Matchescontroller.ControllerMatchesCreate);
+routesMatches.patch('/:id/finish', Matchescontroller.ControllerMatchesFinish);
 
 export default routesMatches;
