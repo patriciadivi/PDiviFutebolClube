62802525ced174c32084afbea9d91bc2d4f7409b
PatriciaDivi <patriciavianad@gmail.com>
2022-08-31 15:35:31 -0300

    fix: :bug: Correção no midlleware erro global status 500 não estava funcionado

--- app/backend/src/utils/globalError.ts
+++ app/backend/src/utils/globalError.ts
@@ -2,15 +2,12 @@ import { Request, Response, NextFunction } from 'express';
 import { CustomerError } from './customerError';
 
 class GlobalError {
-  constructor(private defaultStatus = 500) {
-    this.defaultStatus = defaultStatus;
-  }
-
-  handle(error: CustomerError, _req: Request, res: Response, _next: NextFunction) {
+  // eslint-disable-next-line class-methods-use-this
+  public handle(error: CustomerError, _req: Request, res: Response, _next: NextFunction) {
     if (error instanceof CustomerError) {
       return res.status(error.status).json({ message: error.message });
     }
-    return res.status(this.defaultStatus).json({ message: `>>>> ${error}` });
+    return res.status(500).json({ message: `>>>> ${error}` });
   }
 }
 
