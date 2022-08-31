a6a2433d77846f4347a8a6eaed422e6a70e87d23
PatriciaDivi <patriciavianad@gmail.com>
2022-08-31 15:45:22 -0300

    feat: :sparkles: Camada De controllers
    Criado metodo 'ControllerMatchesCreate' de criação de times
    
    Criado metodo 'ControllerMatchesFinish' de atualização

--- app/backend/src/controllers/matchesController.ts
+++ app/backend/src/controllers/matchesController.ts
@@ -1,21 +1,31 @@
-import { Request, Response } from "express";
+import { Request, Response } from 'express';
 import MatchesService from '../services/matchesService';
 
 class Matchescontroller {
-
   ControllerMatchesAll = async (req: Request, res: Response) => {
     const { inProgress } = req.query;
-    
     if (inProgress) {
       const boolParams = (inProgress === 'true');
-      const resultMatchesModelInprogress = await MatchesService.ServiceMatcherInProgress(boolParams);
-      return res.status(200).json(resultMatchesModelInprogress);
-      
-    } 
-    const resultMatchesServiceAll = await MatchesService.ServiceMatcherAll();
+      const resultMatchesInprogress = await MatchesService.ServiceMatchesInProgress(boolParams);
+      return res.status(200).json(resultMatchesInprogress);
+    }
+
+    const resultMatchesServiceAll = await MatchesService.ServiceMatchesAll();
     return res.status(200).json(resultMatchesServiceAll);
   };
 
+  public ControllerMatchesCreate = async (req: Request, res: Response) => {
+    const { body } = req;
+    const resultMatchesControllerCreate = await MatchesService.ServiceMatchesCreate(body);
+    console.log(resultMatchesControllerCreate);
+    return res.status(201).json(resultMatchesControllerCreate);
+  };
+
+  public ControllerMatchesFinish = async (req: Request, res: Response) => {
+    const { id } = req.params;
+    await MatchesService.ServiceMatchesFinish(+id);
+    return res.status(200).json({ message: 'Finished' });
+  };
 }
 
 export default new Matchescontroller();
