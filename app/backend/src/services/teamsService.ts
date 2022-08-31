df3d3e76d4f88d14805d95ded75c20e4c2d01645
PatriciaDivi <patriciavianad@gmail.com>
2022-08-31 18:32:27 -0300

    refactor: :recycle: Modificações no codigo

--- app/backend/src/services/teamsService.ts
+++ app/backend/src/services/teamsService.ts
@@ -1,14 +1,16 @@
+// import { CustomerError } from '../utils/customerError';
 import TeamsRepository from '../repository/teamsRepository';
 
 class TeamsService {
-
   public ServiceTeamsAll = async () => {
-    const resultTeamsRepository = await TeamsRepository.RepositoryTeamsAll(); 
+    const resultTeamsRepository = await TeamsRepository.RepositoryTeamsAll();
     return resultTeamsRepository;
   };
 
   public ServiceTeamsByAppId = async (id: string) => {
     const resultTeamsRepositoryId = await TeamsRepository.RepositoryTeamsId(id);
+
+    // if (resultTeamsRepositoryId.count < 2) throw new CustomerError(401, 'There is no team with such id!');
     return resultTeamsRepositoryId;
   };
 }
