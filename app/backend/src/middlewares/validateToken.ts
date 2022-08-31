78888d80e69e30d79d49b4ab812c07162a0e2e21
PatriciaDivi <patriciavianad@gmail.com>
2022-08-31 15:40:04 -0300

    feat: :sparkles: Criado Validação par token na rota '/matches' de post

---
+++ app/backend/src/middlewares/validateToken.ts
@@ -0,0 +1,22 @@
+import { Request, Response, NextFunction } from 'express';
+import { CustomerError } from '../utils/customerError';
+import { decryptTokenfunc } from '../helpers/decryptTokenfunc';
+
+const valiteToken = (req: Request, _res: Response, next: NextFunction): void => {
+  const { homeTeam, awayTeam } = req.body;
+  const { authorization } = req.headers;
+
+  if (homeTeam === awayTeam) {
+    throw new CustomerError(401, 'It is not possible to create a match with two equal teams');
+  }
+  if (!authorization) throw new CustomerError(401, 'Token must be a valid token');
+  const token = decryptTokenfunc(authorization as string);
+  const { role } = JSON.parse(JSON.stringify(token));
+  console.log('>>>>>>>>>>>', role);
+
+  if (!role) throw new CustomerError(401, 'Incorrect email or password');
+
+  next();
+};
+
+export default valiteToken;
